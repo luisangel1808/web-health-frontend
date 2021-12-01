541c3aaf2331df3ce333fe6720a4da56743402ea
luaflorez <luaflorez@unicauca.edu.co>
2021-11-30 23:11:19 -0500

    Temporal Fixes en Session and styles in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,24 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FaTimes, FaBars } from "react-icons/fa";
 import { Link, useHistory } from "react-router-dom";
+import { AuthContext } from "../hooks/useAuthentication";
 import Logo from "../statics/logo.png";
 import "../styles/components/Header.css";
 
 const Header = () => {
   const [menu, setMenu] = useState(false);
-  const token = localStorage.token;
+  const {setToken, token} = useContext(AuthContext);
   const history = useHistory();
+
+  useEffect(() => {
+    if(localStorage.getItem('token')){
+      setToken(localStorage.getItem('token'))
+    }
+  }, [])
 
   const close = () =>{
     window.localStorage.removeItem('token')
+    setToken(null)
     history.push('/');
   }
   return (
@@ -19,52 +27,58 @@ const Header = () => {
         <Link to="/">
           <img src={Logo} alt="Web-health" />
         </Link>
+        {token ? (
+          <div className="in">
         <ul className={`menu ${menu ? "active" : "inactive"}`}>
           <li className="header-item">
-            <Link to="/pressure">
+            <Link to="/pressure" onClick={() => setMenu(false)}>
               Registrar
             </Link>
           </li>
           <li className="header-item">
-            <Link to="/pressure-data">
+            <Link to="/pressure-data" onClick={() => setMenu(false)}>
               Registros
             </Link>
           </li>
           <li className="header-item">
-            <Link to="/videos">
+            <Link to="/videos" onClick={() => setMenu(false)}>
               Videos
             </Link>
           </li>
           <li className="header-item">
-            <Link to="/tasks">
+            <Link to="/tasks" onClick={() => setMenu(false)}>
               Tareas
             </Link>
           </li>
           <li className="header-item">
-            <Link to="/teams">
+            <Link to="/teams" onClick={() => setMenu(false)}>
               Comunidad
             </Link>
           </li>
           <li className="header-item">
-            <Link to="/advices">
+            <Link to="/advices" onClick={() => setMenu(false)}>
               Consejos
             </Link>
           </li>
+          <button className="header-item" onClick={()=>close()}>Cerrar sesión</button>
         </ul>
-        <div className="in">
-          {!token ? (
-            <>
-            <Link to="login">
-              <button className="header-item">Ingresar</button>
-            </Link>
-            <Link to="signup">
-              <button className="header-item">Registrarse</button>
-            </Link>
-            </>
-          ) : (
-            <button className="header-item" onClick={()=>close()}>Cerrar sesión</button>
-          )}
         </div>
+        ):(
+        <div className="in">
+        <ul className={`menu ${menu ? "active" : "inactive"}`}>
+          <li className="header-item">
+            <Link to="login">
+              <button className="header-item" onClick={() => setMenu(!menu)}>Ingresar</button>
+            </Link>
+            </li>
+            <li className="header-item">
+            <Link to="signup">
+              <button className="header-item" onClick={() => setMenu(!menu)}>Registrarse</button>
+            </Link>
+            </li>
+          </ul>
+        </div>
+        )}
         <div className="toggle">
           {menu ? (
             <FaTimes onClick={() => setMenu(!menu)} />
